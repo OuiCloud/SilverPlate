4e62432c7fa2f4c448b5a7834e77a88dfa6e00bf
Philippe Menuet Tang <philippe@ouicloud.software>
2025-01-03 16:41:50 +0100

    feat: minimal binary template

---
+++ binary/src/lib.rs
@@ -0,0 +1,8 @@
+//! A minimal Rust binary project with
+//! - copyright
+//! - licences
+//! - code of conduct
+//!
+//! README should be sync with this doc with `cargo-sync-rdme`.
+//! More informations here :
+//! https://github.com/gifnksm/cargo-sync-rdme
